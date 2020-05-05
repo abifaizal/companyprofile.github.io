4062c8f0aff11d0010d3d41880fd82a699a26e82
abifaizal <43515678+abifaizal@users.noreply.github.com>
2020-05-05 21:45:33 +0700

    Add files via upload
    tes

---
+++ index.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap-4.1.3/css/bootstrap.min.css">
+    <!-- style css saya -->
+    <link rel="stylesheet" href="assets/myCSS/myStyle.css">
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Share+Tech" rel="stylesheet">
+
+    <title>Company Profile</title>
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">techGeek</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ml-auto">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Pricing</a>
+            <a class="nav-item nav-link" href="#">Features</a>
+            <a class="nav-item nav-link" href="#">About</a>
+            <a class="nav-item btn btn-primary tombol" href="#">Join Us</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- akhir Navbar -->
+
+    <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Get work done <span>faster</span><br> and <span>better</span> with us</h1>
+        <a href="" class="btn btn-primary tombol">Our work</a>
+      </div>
+    </div>
+    <!-- akhir Jumbotron -->
+
+    <!-- Container content -->
+    <div class="container">
+      <!-- info panel -->
+      <div class="row justify-content-center">
+        <div class="col-10 info-panel">
+          <div class="row">
+            <div class="col-lg">
+              <img src="img/employee.png" alt="employee" class="float-left">
+              <h4>24 Hours!</h4>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="col-lg">
+              <img src="img/hires.png" alt="hires" class="float-left">
+              <h4>High Res</h4>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="col-lg">
+              <img src="img/security.png" alt="security" class="float-left">
+              <h4>Security</h4>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- akhir info panel -->
+
+      <!-- working space -->
+      <div class="row workingspace">
+        <div class="col-lg">
+          <img src="img/workingspace2.jpg" alt="workingspace" class="img-fluid">
+        </div>
+        <div class="col-lg">
+          <h3>You <span>working</span> like at <span>home</span></h3>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A repudiandae nulla blanditiis illum.</p>
+          <a href="" class="btn btn-primary tombol">Gallery</a>
+        </div>
+      </div>
+      <!-- akhir working space -->
+
+      <!-- testimonial -->
+      <section class="testimonial">
+        <div class="row justify-content-center quote">
+          <div class="col-lg-8">
+            <h5>"Bekerja lebih asyik dengan hati yang tenang. Tempatnya juga sangat nyaman dan cozy."</h5>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-6 justify-content-center d-flex">
+            <figure class="figure">
+              <img src="img/img1.png" class="figure-img img-fluid rounded-circle" alt="testi1">
+            </figure>
+            <figure class="figure">
+              <img src="img/img4.jpg" class="figure-img img-fluid rounded-circle aktif" alt="testi1">
+              <figcaption class="figure-caption">
+                <h5>
+                  David
+                </h5>
+                <p>Programmer</p>
+              </figcaption>
+            </figure>
+            <figure class="figure">
+              <img src="img/img2.png" class="figure-img img-fluid rounded-circle" alt="testi1">
+            </figure>
+          </div>
+        </div>
+      </section>
+      <!-- akhir testimonial -->
+
+      <!-- footer -->
+      <div class="row footer">
+        <div class="col text-center">
+          <p>
+            2018 All rights reserved to Faizal Abidin.
+          </p>
+        </div>
+      </div>
+      <!-- akhir footer -->
+    </div>
+    <!-- akhir Container content -->
+    
+
+
+    <script src="assets/Jquery/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="assets/bootstrap-4.1.3/js/bootstrap.min.js"></script>
+  </body>
+</html>
